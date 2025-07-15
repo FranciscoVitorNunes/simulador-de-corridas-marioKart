20fc9790430a49d33423a714b60172182cb73349
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-07-15 14:17:46 -0300

    Concluindo sistema de confronto em cada round.

--- src/index.js
+++ src/index.js
@@ -65,6 +65,11 @@ const player8 = {
 async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 };
+
+async function printResult(player,terrains, sortNumber, fullSkill) {
+    console.log(`${player} rolou um dado de ${terrains} ${sortNumber} e tem um total de ${fullSkill}`);
+}
+
 async function getRandomBlock(){
     let random = Math.random();
     let result;
@@ -87,9 +92,57 @@ async function raceRound(player1, player2) {
             ` Rodada ${round}`
         )
         let block = await getRandomBlock()
-        console.log(
-            `Bloco: ${block}`
-        )
+        console.log(`Bloco: ${block}`)
+
+        let valorSort1 = await rollDice();
+        let valorSort2 = await rollDice();
+
+        let totalSkill1 = 0
+        let totalSkill2 = 0
+
+        if(block=="RETA") {
+            totalSkill1 = player1.VELOCIDADE + valorSort1;
+            totalSkill2 = player2.VELOCIDADE + valorSort2;
+            await printResult(player1.NOME,"VELOCIDADE", valorSort1,totalSkill1)
+            await printResult(player2.NOME,"VELOCIDADE", valorSort2,totalSkill2)
+
+        }
+        if(block=="CURVA") {
+            totalSkill1 = player1.MANOBRA + valorSort1;
+            totalSkill2 = player2.MANOBRA + valorSort2;   
+            await printResult(player1.NOME,"MANOBRABILIDADE", valorSort1,totalSkill1)
+            await printResult(player2.NOME,"MANOBRABILIDADE", valorSort2,totalSkill2)
+
+        }
+        if(block=="CONFRONTO") { 
+            console.log(`${player1.NOME} e ${player2.NOME} entraram em combate.`)
+            totalSkill1 = player1.PODER + valorSort1;
+            totalSkill2 = player2.PODER + valorSort2;   
+            await printResult(player1.NOME,"PODER", valorSort1,totalSkill1)
+            await printResult(player2.NOME,"PODER", valorSort2,totalSkill2)  
+            
+            if(totalSkill1>totalSkill2 && totalSkill2>0){
+                totalSkill2--;
+                console.log(`${player1.NOME} venceu o combate!`)
+            }
+            if(totalSkill2>totalSkill1 && totalSkill1>0){
+                totalSkill1--;
+                console.log(`${player2.NOME} venceu o combate!`)
+            }
+            if(totalSkill1==totalSkill2){
+                console.log("EMPATE, nenhum ponto subtraido!")
+            }
+            
+        }
+        if( totalSkill1 > totalSkill2 ){
+            console.log(`${player1.NOME} marcou ponto!!`)
+            player1.PONTOS++;
+        }else if(totalSkill1 < totalSkill2){
+            console.log(`${player2.NOME} marcou ponto!!`)
+            player2.PONTOS++;   
+        }else if(totalSkill1==totalSkill2){
+            console.log(`Empate !!`)   
+        }
     }
 }
 async function main(){
@@ -97,5 +150,12 @@ async function main(){
         `Iniciando o jogo de corrida entre ${player1.NOME} e ${player2.NOME}...`
     );
     await raceRound(player1, player2);
+    if(player1.PONTOS>player2.PONTOS){
+        console.log(`\n ${player1.NOME} venceu a disputa!`)
+    }else{
+        console.log(`\n ${player2.NOME} venceu a disputa!`)
+    }
+    player1.PONTOS=0;
+    player2.PONTOS=0;
 };
 main();
